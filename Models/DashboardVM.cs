48e4cb82569f7c712621df8e27a46afd22c713fa
Ankita Singh <202ankitasingh@gmail.com>
2026-04-23 22:20:11 +0530

    Signed-off-by: Ankita Singh <202ankitasingh@gmail.com>

---
+++ Models/DashboardVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Wedding_Planner.Models
+{
+    public class DashboardVM
+    {
+        public int TotalBookings { get; set; }
+        public int TotalFeedbacks { get; set; }
+        public int TotalComplaints { get; set; }
+        public int TotalEmails { get; set; }
+
+    }
+}
